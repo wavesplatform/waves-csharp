a0b896f60712a7fedce860d8b1e26e5a0dc02c10
Ed Kashtanov <kashtan4ik@gmail.com>
2022-11-20 21:07:38 +0300

    profile modified

--- sources/Waves.NET/Waves.NET/Profile.cs
+++ sources/Waves.NET/Waves.NET/Profile.cs
@@ -5,6 +5,6 @@
         public const string MainNet = "https://nodes.wavesnodes.com/";
         public const string TestNet = "https://nodes-testnet.wavesnodes.com/";
         public const string StageNet = "https://nodes-stagenet.wavesnodes.com/";
-        public const string Local = "http://127.0.0.1:6869";
+        public const string Private = "http://127.0.0.1:6869";
     }
 }
